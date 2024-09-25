8d52f11c0d19a5b2867159df2b9a6ed0a8b12de3
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-25 09:41:37 +0600

    29-5-2 validation js

---
+++ validation.js
@@ -0,0 +1,16 @@
+const number = 23;
+
+//string, number, boolean
+
+if(typeof number ==='number'){
+	console.log('value is a number');
+}else{
+	console.log('value is not a number');
+};
+
+
+const numbers = [2, 534, 53, 64];
+const student = {name: 'Rongila rafsan', job: 'khawadawa kora'};
+
+console.log(typeof numbers);
+console.log(typeof student);
